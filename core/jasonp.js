ba5d5637a04948f118095744e347a45f74c1f5c9
jonastang <noreply>
2015-06-08 15:29:08 +0000

    Add module jasonp

---
+++ core/jasonp.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/jasonp
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Jasonp
+ * @extends Montage
+ */
+exports.Jasonp = Montage.specialize(/** @lends Jasonp# */ {
+    constructor: {
+        value: function Jasonp() {
+            this.super();
+        }
+    }
+});
